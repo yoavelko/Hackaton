8d8c6dab418e89111be184af2a669226a76bd886
Nusiboy <shahar.nusboim@gmail.com>
2023-06-08 02:43:28 +0300

    responsive intros

--- src/components/PostIntro/PostIntro.jsx
+++ src/components/PostIntro/PostIntro.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./PostIntro.css"
+import "../PostIntro/Browse.css"
+
 
 function PostIntro({ result }) {
   return (
     <div className="intro-container">
-        <div className="intro-header">{`${result?.category}`} |  <Link to={"/PostPage"}>{result?.header}</Link></div>
+        <div className="intro-header">{`${result?.category}`} | <Link to={"/PostPage"}>{result?.header}</Link></div>
       <div className="intro-info">
       <div className="user-pic-container"><img className="intro-user-pic" src={`${result?.profilepic}`}/> <span className="username-intro">User: {`${result?.username}`}</span></div>
       </div>
