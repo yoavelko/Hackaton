c86c1a756793941cb013674eb5c9bb421fa86ef7
Yoavelko <yoavelkobi889@gmail.com>
2023-06-07 16:11:16 +0300

    logsign

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState, useContext } from 'react'
 import './App.css'
 import Login from './components/Login/Login'
 import { LoggedContext } from './context/LoggedContext'
+import { IndexContext } from './context/IndexContext'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
+
 
 function App() {
 
   const [logged, setLogged] = useState('');
+  const [userIndex, setUserIndex] = useState('');
 
   return (
 
     <LoggedContext.Provider value={{ logged, setLogged }}>
-      <div id='app-container'><Login /></div>
+      <IndexContext.Provider value= {{ userIndex, setUserIndex }}>
+        <div id='app-container'><Login /></div>
+      </IndexContext.Provider>
     </LoggedContext.Provider>
   )
 }
