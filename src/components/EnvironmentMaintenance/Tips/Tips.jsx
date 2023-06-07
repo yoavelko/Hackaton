dbb3574e2c353c4e559f50ec8c979c3796199afe
Nusiboy <shahar.nusboim@gmail.com>
2023-06-07 16:11:12 +0300

    fixing routes

--- src/components/EnvironmentMaintenance/Tips/Tips.jsx
+++ src/components/EnvironmentMaintenance/Tips/Tips.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import postData from "../../../post.json"
 import PostIntro from "../../../PostIntro";
+import { useState } from "react";
 function Tips(){
     const [detail, setDetail] = useState(postData.environment.tips)
     return(
