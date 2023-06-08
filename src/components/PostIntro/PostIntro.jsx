b47056989fe060e9ac7dc1718b88e3bac1602b0c
Yoavelko <yoavelkobi889@gmail.com>
2023-06-08 03:33:06 +0300

    merging-fix

--- src/components/PostIntro/PostIntro.jsx
+++ src/components/PostIntro/PostIntro.jsx
@@ -7,15 +7,16 @@ import { IdContext } from "../../context/IdContext";
 
 function PostIntro({ result }) {
 
-    const { id, setId } = useContext(IdContext);
+  const { id, setId } = useContext(IdContext);
+  
 
-    return (
-        <div className="intro-container">
-        <div className="intro-header">{`${result?.category}`} | <Link to={"/PostPage"}>{result?.header}</Link></div>
+  return (
+    <div className="intro-container">
+        <div className="intro-header">{`${result?.category}`} | <Link to={"/ViewPost"} onClick={() => setId(result?.id - 1)}>{result?.header}</Link></div>
       <div className="intro-info">
       <div className="user-pic-container"><img className="intro-user-pic" src={`${result?.profilepic}`}/> <span className="username-intro">User: {`${result?.username}`}</span></div>
       </div>
     </div>
-    );
+  );
 }
 export default PostIntro;
