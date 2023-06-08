d0b54f194d66b9f61974254c555143fe22e5d940
Nusiboy <shahar.nusboim@gmail.com>
2023-06-08 07:29:36 +0300

    emailjs

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,9 @@ import { LoggedContext } from '../../context/LoggedContext'
 import { IndexContext } from '../../context/IndexContext'
 import { LoginContext } from '../../context/LoginContext';
 import usersData from '../../user.json'
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function Login() {
 
@@ -96,6 +98,18 @@ function Login() {
             setCheckErr('Please agree to terms & condition to continue')
         }
     }
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mail', 'template_7jhpypd', form.current, 'yQOjrcezbeaiAmq2V')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
     return (
         <div id='Login-page-conatiner'>
@@ -123,6 +137,7 @@ function Login() {
                 </div>
 
                 <div className='dualContainer'>
+                    <form ref={form} onSubmit={sendEmail}>
                     <h1 className='headers' id='signup-header'>Sign-Up</h1>
                     <div id='signupContainer'>
                         <div className='signup-split'>
@@ -138,18 +153,18 @@ function Login() {
                             <div className='error-msg'>{passErr}</div>
                             <div className='inputContainers'>
                                 <img id='pass-icon' className='icons' width="32" height="32" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAACgUlEQVR4nO1XTWsTYRB+C1EQvxPCvs/zbhII1ko8iKYi+A+8WL2K0Kb+Btur0PYPqAcFS/FnaIygoAet56onsb1a0NjmpJFpZnEt+9V0tSAdGAg7X8/szM5MjNmnjERyDMAsgGckV0h+V14B0AYwIzomb7LWjgPokOxnYQCvSF7edeBms3kAwH2SP9XxF5KPSF6VTD3POyysb2YCwKLq9NXmnjGmMFTwarV6EsBzdbYBYK5YLB5LsxMdAPMANhV0R3wNk3lHHaxJCXaagHPuHMlPmsDLRqNxMLMxBq9dgn+uVCo0Q5LYAlhVEHczGVlrx7V+GyTPR6iMALhF8h2AnjDJZZItkUUkc0HL8UN+pwLA71c/FxP8cUL3L0WBILmg8nZicJJjQbdHNZxmLsG+kpyq1WonhElO6zORTW23q9frxwGsi9w5N5qU/awCWIwBuBwXREGI7E2M7yWV304C0FYAEzFyqXdfst4uk09NbXsxttcVwNMkAB9Fyff9U1HyoNZx9knyUHnfxwIg2RWlcrl8JG8A4lPl3SQA3/4WgCxyA+CDKp3eKwDt3TZhhibdTAIwk/IZvtUgrYQZESlP+0wzDaKQExk6rWAQafCt/gnJp2MG1aRJIgyuHQExHzOKg4ESxQ+VdzSq/yAATVlGUquY5TEik1DKoctI9F5ba28GCvJbnqmsp6WbTA0ekFwyinjV931n9oAKoVLIQXLxnyOoDk6yYDXLPl+QrZZmVyqVjgK4kReOglwyoaN0XRoJwDUAZ2Ri+r5/yFpb06P0QbB2rbVX8gJh9KrZGlIZ+YVz7pLJm5xzo7LPZaXqH5OudvoayScA7niedzb3wPv039Mv2m449ye7LZQAAAAASUVORK5CYII=" alt="private2" />
-                                <input className='inputs' type="text" onChange={(e) => setSignMail(e.target.value)} placeholder='E-Mail' />
+                                <input className='inputs' type="text" name='email' onChange={(e) => setSignMail(e.target.value)} placeholder='E-Mail' />
                             </div>
                             <div className='error-msg'>{mailErr}</div>
                         </div>
                         <div className='signup-split'>
                             <div className='inputContainers'>
                                 <img id='pass-icon' className='icons' width="32" height="32" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABVUlEQVR4nO2WPU7DQBCFXaUDIVuy9701ckWKPQIcIogjIM6Cw01CQRrCBYCcAQ4QqCCClGlAI9aSFRxs5F1T4CdNM7Pr+TzrnxcEvXptyBgzIHkB4Jnkh4sA8ERyLNcO6kRy7KpxReS1APiilcWHgSMBOComUbuYltZV819flz0A60dFckhySnJl40prfdDJEZAcAlhWvGovaZrqLgCmtuG1NJQAMLO5yy4AVlIv3y3JfQvw1gXAu9S11umfAACY2GYzgZAAcGNzE+8ASqkMwLriIVxLzStAHMcJybsfvvW3SZLEXgCMMQMAc1t/ADCKomhHAsAxgEdbu6/667UGAHBWNM+ybG+zLrkSxKkPgLk969G2vUqpk2IKPgCWkpeRb9sbhuGuhXx1DtBWPQCbThbWkomNaj3375ZsUbuYZO7RlJ43teV5yZy6sOULad7Ilvf6d/oEH8s3bDf8m0MAAAAASUVORK5CYII=" alt="private2" />
-                                <input className='inputs' type="text" onChange={(e) => setFirstName(e.target.value)} placeholder='First Name' />
+                                <input className='inputs' type="text" name='firstName' onChange={(e) => setFirstName(e.target.value)} placeholder='First Name' />
                             </div>
                             <div className='inputContainers'>
                                 <img id='pass-icon' className='icons' width="32" height="32" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABVUlEQVR4nO2WPU7DQBCFXaUDIVuy9701ckWKPQIcIogjIM6Cw01CQRrCBYCcAQ4QqCCClGlAI9aSFRxs5F1T4CdNM7Pr+TzrnxcEvXptyBgzIHkB4Jnkh4sA8ERyLNcO6kRy7KpxReS1APiilcWHgSMBOComUbuYltZV819flz0A60dFckhySnJl40prfdDJEZAcAlhWvGovaZrqLgCmtuG1NJQAMLO5yy4AVlIv3y3JfQvw1gXAu9S11umfAACY2GYzgZAAcGNzE+8ASqkMwLriIVxLzStAHMcJybsfvvW3SZLEXgCMMQMAc1t/ADCKomhHAsAxgEdbu6/667UGAHBWNM+ybG+zLrkSxKkPgLk969G2vUqpk2IKPgCWkpeRb9sbhuGuhXx1DtBWPQCbThbWkomNaj3375ZsUbuYZO7RlJ43teV5yZy6sOULad7Ilvf6d/oEH8s3bDf8m0MAAAAASUVORK5CYII=" alt="private2" />
-                                <input className='inputs' type="text" onChange={(e) => setLastName(e.target.value)} placeholder='Last Name' />
+                                <input className='inputs' type="text" name='lastName' onChange={(e) => setLastName(e.target.value)} placeholder='Last Name' />
                             </div>
                             <div className='inputContainers'>
                                 <img  width="32" height="32" id='avatar-img' className='icons' src={avatar} alt="" />
@@ -166,7 +181,8 @@ function Login() {
                         <input type="checkbox" onChange={handleCheck} /> Agree to <span id='termsLink'>terms & conditions</span>
                         <div className='error-msg'>{checkErr}</div>
                     </div>
-                    <button id='signup-submit' className='sub-btns' onClick={handleSign}>Submit</button>
+                    <button id='signup-submit' type='submit'  value="send" className='sub-btns' onClick={handleSign}>Submit</button>
+                    </form>
 
                 </div>
             </div>
